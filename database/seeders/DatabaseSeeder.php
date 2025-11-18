cf354880f69619cf51bc4aac2e220eb251c8b0e2
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2025-11-18 11:46:31 +0100

    Version 1.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Client;
+use App\Models\Produit;
+use App\Models\Commande;
 
-class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+class DatabaseSeeder extends Seeder {
+        use WithoutModelEvents;
+        public function run(): void {
+            Client::factory()->create();
+        }
     }
-}
