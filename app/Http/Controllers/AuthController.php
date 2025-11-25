89ecfe83eca0833d28bc36695e3c0534cd250208
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2025-11-25 11:36:16 +0100

    Version 6

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function auth(Request $request) {
+        if (!$request->isJson()) {
+            return response()->json(['error' => 'Requête JSON attendue'], 401);
+        }
+
+        // authentification email/password
+        $email = $request->json('email');
+        $mdp = $request->json('password');
+        $identifiants = ['email' => $email, 'password' => $mdp];
+
+        if (!Auth::attempt($identifiants)) {
+            return response()->json(['error' => 'Identifiants incorrects'], 401);
+        }
+
+        // création token et retour informations
+        $user = $request->user();
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name
+            ]
+        ]);
+    }
+
+    public function logout(Request $request) {
+        $user = $request->user();
+        $user->tokens()->delete();
+
+        return response()->json([
+            'message' => "Utilisateur {$user->name} déconnecté"
+        ]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\CommandeController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,6 @@ Route::get('/produit/{id}', [ProduitController::class, "getProduit"]);
 
 Route::get('/ajoutCommande/{idClient}/{idProduit}/{qte}', [CommandeController::class, "addCommande"]);
 Route::post('/ajoutCommande', [CommandeController::class, "addCommandeJSON"]);
+
+Route::post('/auth', [AuthController::class, "auth"]);
+Route::get('/logout', [AuthController::class, "logout"])->middleware('auth:sanctum');
