cc763fb43c745e00ac321b97fd73880804c55721
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2025-11-25 10:47:14 +0100

    Version 5

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 use App\Models\Commande;
 
 class CommandeController extends Controller {
-    //
+    function addCommande($idClient, $idProduit, $qte) {
+        $commande = new Commande();
+        $commande->id_client  = $idClient;
+        $commande->id_produit = $idProduit;
+        $commande->quantite   = $qte;
+        $commande->date       = now();
+        $commande->save();
+
+        return response()->json([
+            'status' => 'Commande créée',
+            'data'   => $commande,
+        ]);
+    }
+
+    function addCommandeJSON(Request $request) {
+        $commande = new Commande();
+        $commande->id_client  = $request->get('idClient');
+        $commande->id_produit = $request->get('idProduit');
+        $commande->quantite   = $request->get('qte');
+        $commande->date       = now();
+        $commande->save();
+
+        return response()->json([
+            'status' => 'Commande créée',
+            'data'   => $commande,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\CommandeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,4 +12,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/produits', [ProduitController::class, "liste"]);
 Route::get('/produit/{id}', [ProduitController::class, "getProduit"]);
 
-Route::post('/addCommande/{idClient}/{idProduit}/{qte}', [CommandeController::class, "addCommande"]);
+Route::get('/ajoutCommande/{idClient}/{idProduit}/{qte}', [CommandeController::class, "addCommande"]);
+Route::post('/ajoutCommande', [CommandeController::class, "addCommandeJSON"]);
